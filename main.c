6fab442dfbb168841350f9694c486cfe591e2550
bigcircle <38595887+Bigcirclecircle@users.noreply.github.com>
2019-08-16 23:01:55 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,189 +1,292 @@
 //
 //  main.c
-//  xunxvbiao
-//信仰之跃 来实现我的第一个数据结构
-//  Created by jia jia  on 2019/8/16.
+//  kk
+//
+//  Created by jia jia  on 2019/8/9.
 //  Copyright © 2019年 jia jia . All rights reserved.
 //
 
 #include <stdio.h>
-
-//先用数组来表示一个线性表
-
-#define MAXSIZE 100
-
-typedef int ElemType;
-typedef struct
+#include "string.h"
+#define A 10
+float max(float a , float b)
 {
-    ElemType elem[MAXSIZE];// 线性表占用的数组空间
-    int last;// 线性表中最后一个元素的下标值，空表要置为-1
-}SeqList; // 这花括号后面带个自定义的名字，以后再定义的时候就很舒服了
-//这样就可以定义变量了
-
-/* 初始化线性表*/
-
-//定义操作1 —— 初始化
-
-SeqList *InitList() //无参数形式 这里面如果形参是一级指针会出问题
-{
-    SeqList *p;
-    p= (SeqList *)malloc(sizeof (SeqList));//申请顺序表的空间 前面(SeqList *)，因为p是指针，所以强制类型转换了
-    p->last = -1 ;
-    return p;// 这返回的是一个地址
+    float k ;
+    k= a>b? a:b ;
+    return k;
 }
 
-//操作2 —— 按内容查找 给一个元素，在表里找到一样的，返回位置序号（位置序号是从1开始的），否则返回-1
 
-int Locate(SeqList *L, ElemType e )// 这个地方放第一个参数放L也可以，但是开销大，经济考虑用z地址指针形式
-{
-    int i ;
-    for ( i=1 ; i<=L->last ;i++)
-        if(L->elem[i]==e) return i+1 ;
-    return -1 ;
-}
-
-//操作3 按照序号查找，找到第i个元素
-ElemType GetData(SeqList *L,int i)
-{
-    if(i<=L->last+1) return L->elem[i-1];
-    else printf( "%d id wrong\n",i );
-    return -1;
-}
-
-//操作4 插入操作
-
-#define ERROR -1
-#define OK 1
-int InsList(SeqList *L,int i, ElemType e ) // 这个i就是输入要插入元素的位置，e是插谁
-{
-    int k ;// 循环的时候用它了
-    if ((i<1)||(i>L->last+2)) //判断插入位置是不是在设定范围里,注意，插入操作是可以在最最后插的，所以判断的时候是last+2,因为last+1 是a[n]，也就是最后一个的后一个
-    {
-        printf("插入位置不合法");
-        return (ERROR);
-    }
-    if(L->last>=MAXSIZE-1) // 看看还有没有地方插了
-    {
-        printf("表满了，没地方了");
-        return (ERROR);
-    }
-    
-    for (k=L->last;k>=i-1;k--) //这个是一个循环，先把i后面的元素都往后挪，给i位置腾空，第i个元素的下标是i-1，注意序号和下标的区别
-        L->elem[k+1]=L->elem[k];
-    L->elem[i-1]=e;// 插
-    L->last++ ; //插入之后长度增加
-    return (OK); // ojbk
-    
-    //平均移动次数是 n/2 ,所以时间复杂度是O（n）
-}
-
-//操作5 删除 删除第i个元素，用指针e返回它的值
-
-int DelList(SeqList *L,int i ,ElemType *e)
-{
-    int k ;
-    if (i<1||i>L->last+1)
-    {    printf("非法输入");
-        return (ERROR);
-        
-    }
-    for (k=i;k<=L->last;k++)
-        L->elem[k-1]=L->elem[k];
-    L->last--;
-    return  (OK);
-    
-    // (n-1)/2   O(n)
-}
-
-//例题
-
-void mergeList(SeqList *LA , SeqList *LB ,  SeqList *LC)
-{
-    int i,j,k;
-    i=0;j=0;k=0;
-    
-    while (i<=LA->last&&j<=LB->last) // 比较
-    {  if(LA->elem[i]<LB->elem[j])
-         { LC->elem[k]=LA->elem[i];
-             i++;k++;
-          }
-        else
-        { LC->elem[k]=LB->elem[j];
-            j++;k++;}
-    }
-    
-    while(i<=LA->last) // 吃剩饭
-    {
-        LC->elem[k] = LA->elem[i];
-        i++;k++;
-    }
-    
-    while(j<=LB->last)
-    {
-        LC->elem[k] = LB->elem[j];
-        j++;k++;
-    }
-    
-    LC->last = LB->last+LA->last+1;  // 新下标， 这是下标，所以要+1
-}
 int main(int argc, const char * argv[]) {
+    // insert code here...
+//    printf("Hello, World!\n");
+//
+//    long int a= 2e3,b=2E2 ;char c ;
+//    c='\123';
+//
+//
+//   for (;c=getchar()!='\n';){}
+//    printf("%c",c);
     
-/*
-    SeqList L;  //第一种定义方式
-    //scanf("%d",&L.elem[1]);  //这个直接输入是不好用的 奇怪了 但是编译OK
-      printf("%d",L.elem[1]);
-      printf("%d",L.elem[L.last]); // 最后一个元素怎么写，总共有last+1 个元素
-    //输出测试
- */
- 
- /*
-    SeqList La , *L; // 第二种定义方式
-    L= &La;
-  printf("%d",L->elem[1]); // 用指针怎么输出
-*/
+//    printf("%c",c);
+    
+//    printf("\101");
+    
+//   printf("\x41");
+    
+//     printf("%d%d",c);
+    
+//    int i = 1 ,sum= 0;
+//
+//    while(i<=100){
+//
+//
+//        sum+=i;
+//
+//         i++;
+//
+//        /*上面这2个货不能互换，换了会差100，因为多加一个101，少加一个1*/
+//    }
+//    printf("sum=%d",sum);
+    
+    
+//    int sum=0 ;
+//    //for (int i =1 ;i<=100;i++)
+//    for (int i=100 ; i>=1;i--)
+//    {sum+=i;}
+//    printf("sum=%d",sum);
+    //    return 0;}
+    
+//    for (;c=getchar()!='\n';){}
+//        printf("%c",c);
+
+    
+//   long sum=0;
+//    for (int i =10 ;i<=150;i++)
+//    {
+//        if(i%2!=0)
+//        {sum= sum+i*i;}
+//
+//    }
+//    printf("sum=%d",sum );
+//
+    
+//    float sum=0,s=1 ;
+//    int j;
+//
+//
+//        for(j=1;j<=10;j++)
+//        {
+//            s=s*j;
+//            sum= sum + 1/s;}
+//
+//
+//    printf("sum=%.5f",sum );
+    
+//    int i,j,k;
+//
+//    int a =999;
+//    while (a>=100)
+//    {
+//        i=a%10;
+//        k=a/100;
+//        j= a/10-10*k;
+//
+//        if(a==i*i*i+j*j*j+k*k*k){
+//            printf ("shuixianis %d",a);
+//        }
+//        a--;
+//
+//    }
+//    int k=5 ;
+//    for(int i=1; i<=k;i++ )
+//    {
+//
+//        for(int l=1;l<6-i;l++)
+//        { putchar(' ');
+//            //puts(" ");
+//        }
+//
+//        for(int j=1; j<=2*i-1;j++ )
+//        {
+//
+//            printf("%d",j);
+//
+//        }
+//        printf("\n");
+//    }
+    
+    
     
    
-    //练习开始 使用之前定义的操作
-    int i ;
-    SeqList *p ;
-    p=InitList();
-   // printf("%d\n",p->last);//如果返回-1 那说明初始化成功了
     
-    for(i=0;i<10;i++) p->elem[i]=i+50; //给表前十个元素赋值
-    p->last = i-1;//这个就是上边的i ，指针指向表最后一个有效元素，应该是9 ， 指的元素应该是59
-    //printf( "%d\n", p->last); // 最后一个元素的位置
-    //printf( "%d\n",p->elem[p->last]);//输出最后一个元素
-    i = Locate( p, 54);//找到元素54在表里的位置
-    // printf("%d\n",i);输出位置
-// printf("%d\n",GetData(p, 5));// 找到位置是5的元素
+//    float a = 7  ;float b= 9;
+//
+//    printf ("%f",max(a,b));
     
-    //练习题 将两个有序递增序列合成一个顺序表，LA(1,2,5) LB(1,3,4,4)--> LC(1,1,2,3,4,4,5)
+    //求阶乘的和 factsum 阶乘的和可能是一个很大的数，所以定义的时候要用long来定义，因为不再是常用的int char void，所以放在后面的时候要在主函数里声明函数
+//
+//    long fact(int n);
+//
+//    int k ;
+//    printf("你想求谁的阶乘？") ;
+//    scanf("%d",&k);
+//    printf("%ld\n",fact(k) ) ;
+//
+    //来玩递归
+//    int m ,n ,k ;
+//    scanf("%d%d",&m,&n);
+//    if (m<n)
+//        k=n;n=m;m=k;
+//     printf("%d\n",gcd(m,n) ) ;
+//
     
-    SeqList *p1 ;
-    SeqList *p2 ;
-    SeqList *p3 ;
-    p1 = InitList();
-    p2 = InitList();
-    p3 = InitList();
-    int a[3]= {1,2,5};
-    int b[4] = {1,3,4,4};
-    for (int h=0 ; h< 3;h++)
-        p1->elem[h]=a[h];
-    p1->last = 2;
+    //超绝可爱汉诺塔
+//    int n ;
+//    printf("几层塔？");
+//    scanf("%d",&n);
+//    void hanoi(int n ,char a, char b, char c);
+//    hanoi(n, 'A', 'B', 'C');
     
-    for (int g=0;g<4;g++)
-        p2->elem[g]=b[g];
-      p2->last = 3;
+    //printf ("%d", sizeof(long));
+    void yh(int a[][A],int n);
+    void max1(int a[],int b);
+    void sort1(int a[],int b);
+    int a[5]= {2,3,4,1,2};
+  // sort1(a,5);
     
-    mergeList( p1 ,p2 , p3);
-  
+    int b[5][A];
+    //yh(b,5);
     
-    for (int f= 0;f<p3->last+1;f++)
-       printf("    %d" , p3->elem[f]);
-    
-    //定义 赋值 循环比元素 谁小谁入列 剩下的统统插到尾巴里
-       //定义函数去了 见mergelist
+    // 回文数
     
     
-    return 0;
+    //命令行参数
+//    int i=1,n=argc;
+//    printf("argc=%d\n",n);
+//    printf("\n程序名:%s",*argv);
+//    argv++;n--;
+//    while (i<=n)
+//    {
+//        printf("\n参数%d:%s",i,*argv);
+//        i++;
+//        argv++;
+//    }
+    
+    //指针法降序排列
+//    int d[] = {8,5,2,7,7};
+//    //printf("%d",sizeof(d)/4);
+//    int n =sizeof(d)/4;
+//    int i,*p2,temp,*p1=d;
+//    for(;p1<d+n-1;p1++)//最终数组存放准备
+//        for(p2=p1+1;p2<d+n;p2++)//遍历比较
+//            if(*p1<*p2)
+//            {temp=*p1;*p1=*p2;*p2=temp;}
+//    for(i=0,p1=d;i<n;i++,p1++) printf("%3d" ,*p1);
+//    p1--;
+//    printf("max= %d, min= %d", *(p1-n+1),*p1);
+    
+    //频度计数
+    
+    int i,j,k,sum=0;
+    int n=10,a,count= 0;
+    for (i=0;i<n;i++)
+    
+return 0;}
+
+
+//long fact(int n) //阶乘函数
+//{
+//    long fact=1;
+//    for (int i=1 ;i<=n;i++)
+//        fact=fact*i ;
+//
+//    return fact ;
+//}
+//
+//long factsum(int n)
+//
+//{
+//    long fs=0;
+//    long fact(int n);
+//    for (int i=1 ;i<=n;i++)
+//        fs=fs+ fact(i);
+//    return fs ;
+//}
+
+long fact(int n)
+{
+    
+    if (n==0)
+        return 1 ;
+    else
+        return(n*fact(n-1));
+}
+
+//辗转相除法 爱的魔力转圈圈
+
+//int gcd(int m, int n)
+//{
+//
+//    if(m%n == 0 )
+//        return n;
+//     else
+//        return gcd(n,m%n);
+//}
+
+void hanoi(int n , char a ,char b, char c )
+
+{
+    if (n==1)
+        printf("%c-->%c\n",a,c);
+    else
+    { hanoi(n-1, a, c, b);
+        printf("%c-->%c\n",a,c);
+        hanoi(n-1, b, a, c);}
+    }
+
+void max1( int a[], int b )
+{
+    int k=a[0] ;
+    for (int i =1 ;i<b;i++)
+        {
+        k= k>a[i]? k:a[i];
+        }
+    printf ("%d",k);
+}
+
+void sort1 (int a[],int b)
+{
+    int i,j,k,t;
+    //for (j=0;j<b;j++)
+    for (t=0;t<b-1;t++)
+    {  k=t ;
+        for (i=t+1;i<b;i++)
+        if (a[k]<a[i])
+          k=i;
+       // printf("%d",k);
+        if (k!=t){ j= a[t];a[t]=a[k];a[k]=j;}
+    }
+    
+    for (int c=0;c<b;c++)
+    {printf ("%d",a[c]);}
+    
+}
+
+void yh(int a[][A], int n ) //杨辉三角
+{
+    int i,j;
+    for (i=0;i<n;i++)
+    {
+        a[i][0]=1; a[i][i]=1 ;
+    }
+    
+   for (i=2;i<n;i++)
+       for(j=1;j<n;j++)
+       {a[i][j]=a[i-1][j]+a[i-1][j-1];}
+    
+    for (i=0; i<n;i++)
+    {   for(j=0;j<=i;j++)
+            printf("%-6d",a[i][j]);
+        printf("\n");}
 }
